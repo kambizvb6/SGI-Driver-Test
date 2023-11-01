6dbabd621b2f4730198062c874fed9d66cfcf1ce
kambizvb6 <kambizvb6@gmail.com>
2023-11-01 13:19:45 -0600

    Initial Commit

---
+++ SGI-Driver-Test/SGI_Driver_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SGI_Driver_TestApp.swift
+//  SGI-Driver-Test
+//
+//  Created by Kambiz on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct SGI_Driver_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
