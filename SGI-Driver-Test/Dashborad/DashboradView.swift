f617ccbb39036e78e95dae95f9ab34abd4719789
kambizvb6 <kambizvb6@gmail.com>
2023-11-25 17:34:09 -0600

    DashboradView, including Slider, ProgressBar, DashboradMenuItem

---
+++ SGI-Driver-Test/Dashborad/DashboradView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboradView.swift
+//  SGI-Driver-Test
+//
+//  Created by Kambiz on 2023-11-23.
+//
+
+import SwiftUI
+
+struct DashboradView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboradView()
+}
